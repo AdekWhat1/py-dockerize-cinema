bafa1b22666fbe42201d0443e904eced0dd648ce
Den <mr.denisyakubenko@gmail.com>
2026-08-09 16:35:17 +0200

    feat: full dockerization of DRF cinema project

---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,20 @@
+import time
+from django.core.management import BaseCommand
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2Error
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("waiting for db...")
+        db_up = False
+
+        while not db_up:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Database unavailable, waiting...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
